71ea8fb8b7aba061c1c816cf678c499ecae18ef9
newmri <newmri@naver.com>
2025-07-22 21:48:52 +0900

    history에 timestamps 추가, 인덱스 추가

--- server/models/drinktracker/histories/drinkhistory.js
+++ server/models/drinktracker/histories/drinkhistory.js
@@ -1,24 +1,29 @@
 const mongoose = require("mongoose");
 
-const DrinkHistorySchema = new mongoose.Schema({
-  type: {
-    type: String,
-    required: true,
-  },
-  amount: {
-    type: Number,
-    required: true,
-    min: 1,
-    validate: {
-      validator: Number.isInteger,
-      message: "{VALUE} 는 정수가 아닙니다.",
+const DrinkHistorySchema = new mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+      min: 1,
+      validate: {
+        validator: Number.isInteger,
+        message: "{VALUE} 는 정수가 아닙니다.",
+      },
+    },
+    time: {
+      type: Date,
+      required: true,
+      default: Date.now,
     },
   },
-  time: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
+
+DrinkHistorySchema.index({ time: 1 });
 
 module.exports = mongoose.model("DrinkHistory", DrinkHistorySchema);
--- server/models/drinktracker/histories/goalhistory.js
+++ server/models/drinktracker/histories/goalhistory.js
@@ -1,29 +1,34 @@
 const mongoose = require("mongoose");
 
-const GoalHistorySchema = new mongoose.Schema({
-  weight: {
-    type: Number,
-    required: true,
-    min: 0,
-    validate: {
-      validator: Number.isInteger,
-      message: "{VALUE} 는 정수가 아닙니다.",
+const GoalHistorySchema = new mongoose.Schema(
+  {
+    weight: {
+      type: Number,
+      required: true,
+      min: 0,
+      validate: {
+        validator: Number.isInteger,
+        message: "{VALUE} 는 정수가 아닙니다.",
+      },
+    },
+    goal: {
+      type: Number,
+      required: true,
+      min: 1,
+      validate: {
+        validator: Number.isInteger,
+        message: "{VALUE} 는 정수가 아닙니다.",
+      },
+    },
+    time: {
+      type: Date,
+      required: true,
+      default: Date.now,
     },
   },
-  goal: {
-    type: Number,
-    required: true,
-    min: 1,
-    validate: {
-      validator: Number.isInteger,
-      message: "{VALUE} 는 정수가 아닙니다.",
-    },
-  },
-  time: {
-    type: Date,
-    required: true,
-    default: Date.now,
-  },
-});
+  { timestamps: true }
+);
+
+GoalHistorySchema.index({ time: 1 });
 
 module.exports = mongoose.model("GoalHistory", GoalHistorySchema);
