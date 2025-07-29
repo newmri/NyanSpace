bd4a5b1097e2b0ecc414a2b5afde43cc45d1f5ca
newmri <newmri@naver.com>
2025-07-29 20:49:16 +0900

    세션 유지 30일로 변경

--- server/index.js
+++ server/index.js
@@ -34,7 +34,7 @@ app.use(
     saveUninitialized: false,
     cookie: {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24, // 1일
+      maxAge: 1000 * 60 * 60 * 24 * 30, // 30일
     },
   })
 );
