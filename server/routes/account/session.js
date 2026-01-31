f6842b06f2068bfd67d25f54a4321c0db33b6ccb
newmri <newmri@naver.com>
2026-02-01 00:19:45 +0900

    로그인 안했을 때 세션 에러 남는거 수정

--- server/routes/account/session.js
+++ server/routes/account/session.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const Account = require("../../models/account/account");
-const { requireSignIn } = require("../middlewares/auth");
 
-router.get("/session", requireSignIn, async (req, res) => {
+router.get("/session", async (req, res) => {
   try {
     const accountId = req.session.accountId;
+
+    if (!accountId) {
+      return res.json({ account: null });
+    }
+
     const account = await Account.findById(accountId);
     if (!account) {
       return res.status(404).json({ error: "사용자를 찾을 수 없습니다." });
